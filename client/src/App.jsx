9689223b112214211ae6a4670433af8f562fd6b2
vishal prajapati <vishalprajapati43968@gmail.com>
2025-08-19 15:32:58 +0530

    setup react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
 import { Provider } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import store from "./store";
+import Layout from "./components/Layout";
 
-import PostDetails from "./screen/PostDetails";
-import Login from "./screen/Login";
-import Regiser from "./screen/Register";
-import Home from "./screen/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+]);
+
 const App = () => {
   return (
     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Regiser />} />
-      </Routes>
+      <RouterProvider router={router} />
     </Provider>
   );
 };
